2508794c78cfb55535046db08206b4d04ccbaa3d
dc <dc@dcs-MacBook-Pro.local>
2020-01-19 18:25:08 +0800

    repair some upgrade logic code.

--- src/renderer/components/Home.vue
+++ src/renderer/components/Home.vue
@@ -138,7 +138,7 @@
     },
     mounted() {
       this.$store.dispatch('showLoadingModals');
-      const url = 'https://api.github.com/repos/TesraSupernet/TesraWallet/releases/latest';
+      const url = 'https://api.github.com/repos/user00000001/TWallet/releases/latest';
       const version = this.version;
       axios.get(url).then(res => {
         this.$store.dispatch('hideLoadingModals');
